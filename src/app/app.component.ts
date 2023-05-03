15f8c9a3fc760190ca96ce4a06139ad6d7e01732
Maikolcode <maikol320434@gmail.com>
2023-05-03 14:22:23 -0500

    add report manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { EditorConfig, init } from '@grammarly/editor-sdk';
+import { EditorConfig, GrammarlyEditorPluginElement, init } from '@grammarly/editor-sdk';
 import { quillsInformation } from 'src/mock-data';
-import { QuillTextArea } from './models/quill.model';
+import { GrammarlyEditor, QuillTextArea } from './models/quill.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  clientId = 'your_api_key'; // 👈 your grammarly client id here
+  clientId = 'your_api_key'; // 👈 your grammarly client id here;
   quills: QuillTextArea[] = quillsInformation;
   toggle: boolean = false;
-  editors: any[] = [];
+  editors: GrammarlyEditor[] = [];
 
   ngOnInit() {
     this.initGrammarly();
@@ -31,14 +31,37 @@ export class AppComponent implements OnInit {
       const Grammarly = await init(this.clientId, config);
       const textAreas = document.querySelectorAll('.ql-editor') as NodeList;
 
-      textAreas.forEach((item) => {
+      textAreas.forEach((item, index) => {
         const quillArea = item as HTMLElement;
         if (quillArea) {
           let editor = Grammarly.addPlugin(quillArea);
-          this.editors.push(editor);
+          this.addEventsToEditors(editor, index);
+          this.editors.push(
+            {
+              document: {},
+              session: {},
+              editor
+            }
+          );
         }
       });
     })();
+  }
+
+  addEventsToEditors(editor: GrammarlyEditorPluginElement, index: number): void {
+    editor.addEventListener(
+      'document-stats',
+      (event) => this.grammarLogs(event, index)
+    );
+    editor.addEventListener(
+      'session-stats',
+      (event) => this.grammarLogs(event, index)
+    );
+  }
+
+  grammarLogs(event: any, index: number) {
+    if(event.type === 'document-stats') this.editors[index].document = event.detail;
+    if(event.type === 'session-stats') this.editors[index].session = event.detail;
   }
 
   toggleGrammarly(): void {
@@ -47,6 +70,26 @@ export class AppComponent implements OnInit {
   }
 
   disconnectEditors(): void {
-    this.editors.forEach((editor) => editor.disconnect());
+    this.editors.forEach((editor) => editor.editor.disconnect());
+  }
+
+  showLogs(): void {
+    let reportCompleted = {
+      characters: 0,
+      wordsCount: 0,
+      wordsAnalyzed: 0,
+      wordsChecked: 0,
+      suggestionsCount: 0
+    }
+    this.editors.forEach(items => {
+      console.log(items);
+      reportCompleted.characters += items.document.wordsCount !== 0 ? items.document.charsCount : 0;
+      reportCompleted.wordsCount += items.document.wordsCount;
+      reportCompleted.wordsAnalyzed += items.session.wordsAnalyzed;
+      reportCompleted.wordsChecked += items.session.wordsChecked;
+      reportCompleted.suggestionsCount += items.document.suggestionsCount;
+    })
+
+    console.log(reportCompleted);
   }
 }
--- src/app/models/quill.model.ts
+++ src/app/models/quill.model.ts
@@ -1,3 +1,12 @@
+import { GrammarlyEditorPluginElement } from "@grammarly/editor-sdk";
+
 export interface QuillTextArea {
   text: string
+}
+
+
+export interface GrammarlyEditor {
+  editor: GrammarlyEditorPluginElement;
+  document: any;
+  session: any;
 }
